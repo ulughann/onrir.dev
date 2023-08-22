dbb0f5ce36e9b55160be95ef0efd90081528688a
onrirr <iscosmos@outlook.com>
2023-08-22 15:21:02 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://onrirr.github.io',
+  base: '/portfolio', // remove when custom domain
+});
