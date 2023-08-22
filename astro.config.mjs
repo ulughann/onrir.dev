ebbb5faf91a3e04146ec58bb4997b4225c881597
onrirr <iscosmos@outlook.com>
2023-08-22 15:23:38 +0300

    tailwind init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onrirr.github.io',
-  base: '/portfolio', // remove when custom domain
+  base: '/portfolio' // remove when custom domain
+  ,
+  integrations: [tailwind()]
 });
