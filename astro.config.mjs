7cbed7d73084ba1c04e7dfcd67ffefc03752c542
onrirr <ulughann@gmail.com>
2023-12-22 18:43:57 +0300

    Major ovarhaul (incomplete)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
 
 // https://astro.build/config
 export default defineConfig({
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,50 +2,5 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        primary: "rgb(167, 161, 250)",
-        background: "#1b1f32",
-				text: "#ddc5d9"
-      },
-    },
-    fontFamily: {
-      body: [
-        "Inter",
-        "ui-sans-serif",
-        "system-ui",
-        "-apple-system",
-        "system-ui",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "Noto Sans",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-        "Noto Color Emoji",
-      ],
-      sans: [
-        "Inter",
-        "ui-sans-serif",
-        "system-ui",
-        "-apple-system",
-        "system-ui",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "Noto Sans",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-        "Noto Color Emoji",
-      ],
-    },
-  },
   plugins: [],
 };
