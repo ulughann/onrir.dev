77d58abd96437813c12c456167516d89c3aa06d2
onrirr <ulughann@gmail.com>
2024-01-17 19:33:47 +0300

    defined new color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9137A8",
+        secondary: "#CB384A",
+        accent: "#C95C85",
+        background: "#1E1C45",
+        "background-gray": "#323239",
+        text: "#D1CFEC",
+      },
+    },
+  },
 };
